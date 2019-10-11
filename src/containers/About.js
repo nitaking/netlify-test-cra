1b07a757a8f9fcfd4f65954783e63815c666ffac
nitaking <10850034+nitaking@users.noreply.github.com>
2019-10-11 20:25:43 +0900

    Add: Meta: seo

--- src/containers/About.js
+++ src/containers/About.js
@@ -6,19 +6,21 @@ const About = () => (
     <Helmet
       title="About"
       meta={[
+        // twitter
         { name: 'twitter:card', content: 'summary' },
         { name: 'twitter:title', content: 'About' },
         { name: 'twitter:description', content: 'description of About' },
         { name: 'twitter:image', content: 'http://path/to/image' },
+        // seo
+        { name: 'google-site-verification', content: 'wN9NyQArWBo-TmqcpD5QP8wzvRSSJ1pAkS9eFUH-I3E' },
+        // ogp
         { property: 'og:title', content: 'About' },
         { property: 'og:type', content: 'website' },
         { property: 'og:url', content: 'http://path/to/this/url' },
         { property: 'og:image', content: 'http://path/to/image' },
         { property: 'og:description', content: 'description of About' },
       ]}
-    >
-        <meta name="google-site-verification" content="wN9NyQArWBo-TmqcpD5QP8wzvRSSJ1pAkS9eFUH-I3E" />
-    </Helmet>
+    />
   </div>
 );
 
